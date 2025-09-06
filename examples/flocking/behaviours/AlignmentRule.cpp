8e35d76e67af2ef726232c449e98f6c928c7c2aa
Max Schroeder <max.schroeder@mymail.champlain.edu>
2025-09-06 18:23:24 -0400

    Adding code to AlignmentRule.cpp and CohesionRule.cpp
    Errors relating to stl_uninitialized and vector?

--- examples/flocking/behaviours/AlignmentRule.cpp
+++ examples/flocking/behaviours/AlignmentRule.cpp
@@ -4,9 +4,26 @@
 Vector2f AlignmentRule::computeForce(const std::vector<Boid*>& neighborhood, Boid* boid) {
   // Try to match the heading of neighbors = Average velocity
   Vector2f averageVelocity = Vector2f::zero();
-
+  float count = 0;
   // todo: add your code here to align each boid in a neighborhood
   // hint: iterate over the neighborhood
+  float radius = boid->getDetectionRadius();
+  for (auto i : neighborhood) {
+    Boid currentBoid = *i;
+    Vector2f distance = currentBoid.getPosition() - boid->getPosition();
+    float mag = distance.getMagnitude();
+    if (mag <= radius) {
+      averageVelocity += currentBoid.getVelocity();
+      count++;
+    }
 
+  }
+  if (count == 0) {
+     return {0,0};
+  }
+  averageVelocity /= count;
+  averageVelocity *= weight;
+  //weighted average?
+  //clamp to radius?
   return Vector2f::normalized(averageVelocity);
 }
--- examples/flocking/behaviours/CohesionRule.cpp
+++ examples/flocking/behaviours/CohesionRule.cpp
@@ -3,11 +3,27 @@
 
 Vector2f CohesionRule::computeForce(const std::vector<Boid*>& neighborhood, Boid* boid) {
   Vector2f cohesionForce;
-
+  Vector2f centerMass = {0,0};
+  float count = 0;
+  float radius = boid->getDetectionRadius();
   // todo: add your code here to make a force towards the center of mass
-  // hint: iterate over the neighborhood
-
-  // find center of mass
+  for (auto i : neighborhood) {
+    if (i != boid) {
+      Boid currentBoid = *i;
+      Vector2f distance = currentBoid.getPosition() - boid->getPosition();
+      float mag = distance.getMagnitude();
+      if (mag <= radius) {
+        centerMass += currentBoid.getPosition();
+        count++;
+      }
+    }
+  }
+  if (count<=0) {
+    return {0,0};
+  }
+  cohesionForce = centerMass/count;
+  cohesionForce -= boid->getPosition();
+  cohesionForce *= weight;
 
   return cohesionForce;
 }
