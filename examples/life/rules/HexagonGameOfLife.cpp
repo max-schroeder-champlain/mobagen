6543f75b1731cca8a43403507e6d70b7ed0fd8c5
Max Schroeder <max.schroeder@mymail.champlain.edu>
2025-09-13 19:15:32 -0400

    Finishing Hexagon

--- examples/life/rules/HexagonGameOfLife.cpp
+++ examples/life/rules/HexagonGameOfLife.cpp
@@ -8,16 +8,15 @@ void HexagonGameOfLife::Step(World& world) {
     for (int j = 0; j < world.SideSize(); j++) {
       Point2D currentPoint = Point2D(i,j);
       int neighborsCount = CountNeighbors(world, currentPoint);
-      if (neighborsCount < 2) {
-        world.SetNext(currentPoint, false);
+      if (world.Get(currentPoint)) {
+        if (neighborsCount < 2) {
+          world.SetNext(currentPoint, false);
+        }
+        else if (neighborsCount >= 3) {
+          world.SetNext(currentPoint, false);
+        }
       }
-      else if ((neighborsCount == 2 || neighborsCount == 3) && world.Get(currentPoint)) {
-        world.SetNext(currentPoint, true);
-      }
-      else if (neighborsCount > 3) {
-        world.SetNext(currentPoint, false);
-      }
-      else if (neighborsCount == 3 && !world.Get(currentPoint)) {
+      if (neighborsCount == 2) {
         world.SetNext(currentPoint, true);
       }
     }
@@ -27,59 +26,27 @@ int HexagonGameOfLife::CountNeighbors(World& world, Point2D point) {
   //If y == even Need to check up then left
   // if y != even need to check up then right
   int count = 0;
-  /*Point2D newPoint;
-  if ((point.y %=2 ) == 0) {
-    newPoint = Point2D(0,1);
-    newPoint += point;
-    if (world.Get(newPoint)){
+  if (world.Get(point.Left()))
+    count++;
+  if (world.Get(point.Right()))
+    count++;
+  if (world.Get(point.Up()))
+    count++;
+  if (world.Get(point.Down()))
+    count++;
+  int temp = point.y %= 2;
+  if (temp == 0) {
+    if (world.Get(point.Down().Left()))
       count++;
-    }
-    newPoint = Point2D(-1,1);
-    newPoint += point;
-    if (world.Get(newPoint)){
+    if (world.Get(point.Up().Left()))
       count++;
-    }
-    if (world.Get(newPoint)){
-      count++;
-    }
-    newPoint = Point2D(-1,1);
-    newPoint += point;
-    if (world.Get(newPoint)){
-      count++;
-    }
   }
-    else {
-      newPoint = Point2D(0,1);
-      newPoint += point;
-      if (world.Get(newPoint)){
-        count++;
-      }
-      newPoint = Point2D(1,1);
-      newPoint += point;
-      if (world.Get(newPoint)){
-        count++;
-      }
-      newPoint = Point2D(0,-1);
-      newPoint += point;
-      if (world.Get(newPoint)){
-        count++;
-      }
-      newPoint = Point2D(1,1);
-      newPoint += point;
-      if (world.Get(newPoint)){
-        count++;
-      }
-    }
-    newPoint = Point2D(1,0);
-    newPoint += point;
-    if (world.Get(newPoint)){
+  else {
+    if (world.Get(point.Down().Right()))
       count++;
-    }
-    newPoint = Point2D(-1,0);
-    newPoint += point;
-    if (world.Get(newPoint)){
+    if (world.Get(point.Up().Right()))
       count++;
-    }*/
+  }
 
   return count;
 }
