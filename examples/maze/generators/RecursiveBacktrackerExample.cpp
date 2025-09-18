ebd8a489c9686818debf1e06b794b71192f75575
Max Schroeder <max.schroeder@mymail.champlain.edu>
2025-09-17 21:25:43 -0400

    Finally got backtracking working

--- examples/maze/generators/RecursiveBacktrackerExample.cpp
+++ examples/maze/generators/RecursiveBacktrackerExample.cpp
@@ -29,7 +29,46 @@ struct Maze {
 };
 
 bool RecursiveBacktrackerExample::Step(World* w) {
-  // todo: implement this
+  Point2D neighbor, currentPoint;
+  //currentPoint = Point2D(stack.back().x, stack.back().y);
+
+  if (stack.empty()) {
+    stack.push_back(randomStartPoint(w));
+  }
+  else {
+    visited[stack.back().x][stack.back().y] = true;
+    w->SetNodeColor(stack.back(), Color::Red);
+    std::vector<Point2D> visitables = getVisitables(w, stack.back());
+    if (visitables.empty()) {
+      w->SetNodeColor(stack.back(), Color::Black);
+      stack.pop_back();
+      return true;
+    }
+    neighbor = visitables.front();
+    if (visitables.size() != 1) {
+      Random random;
+      int num = random.Range(0, visitables.size()-1);
+      neighbor = visitables[num];
+    }
+    if (neighbor == stack.back().Up()) {
+      w->SetNorth(stack.back(), false);
+      w->SetSouth(stack.back().Up(), false);
+    }
+    else if (neighbor == stack.back().Down()) {
+      w->SetSouth(stack.back(), false);
+      w->SetNorth(stack.back().Down(), false);
+    }
+    else if (neighbor == stack.back().Left()) {
+      w->SetWest(stack.back(), false);
+      w->GetEast(stack.back().Left());
+    }
+    else if (neighbor == stack.back().Right()) {
+      w->SetEast(stack.back(), false);
+      w->SetWest(stack.back().Right(), false);
+    }
+    stack.push_back(neighbor);
+    return true;
+  }
   return false;
 }
 
@@ -60,6 +99,25 @@ std::vector<Point2D> RecursiveBacktrackerExample::getVisitables(World* w, const 
   std::vector<Point2D> visitables;
 
   // todo: implement this
-
+  Point2D pointNorth = p.Up();
+  Point2D pointSouth = p.Down();
+  Point2D pointWest = p.Left();
+  Point2D pointEast = p.Right();
+  bool northGood = (pointNorth.x >= -sideOver2) && (pointNorth.x <= sideOver2) && (pointNorth.y >= -sideOver2) && (pointNorth.y <= sideOver2);
+  if (northGood && !visited[pointNorth.x][pointNorth.y]) {
+    visitables.push_back(pointNorth);
+  }
+  bool southGood = (pointSouth.x >= -sideOver2) && (pointSouth.x <= sideOver2) && (pointSouth.y >= -sideOver2) && (pointSouth.y <= sideOver2);
+  if (southGood && !visited[pointSouth.x][pointSouth.y]) {
+    visitables.push_back(pointSouth);
+  }
+  bool westGood = (pointWest.x >= -sideOver2) && (pointWest.x <= sideOver2) && (pointWest.y >= -sideOver2) && (pointWest.y <= sideOver2);
+  if (westGood && !visited[pointWest.x][pointWest.y]) {
+    visitables.push_back(pointWest);
+  }
+  bool eastGood = (pointEast.x >= -sideOver2) && (pointEast.x <= sideOver2) && (pointEast.y >= -sideOver2) && (pointEast.y <= sideOver2);
+  if (eastGood && !visited[pointEast.x][pointEast.y]) {
+    visitables.push_back(pointEast);
+  }
   return visitables;
 }
